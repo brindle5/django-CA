1ed0bc8dc1f7d9172c8db99752620e9069d97e42
brindle5 <>
2023-05-11 17:19:16 +0000

    Alter settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-brindle5-django-ca-hemts1qbzj.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-brindle5-django-ca-hemts1qbzj.us2.codeanyapp.com',
+                 'hello---django-app.herokuapp.com']
 
 
 # Application definition
